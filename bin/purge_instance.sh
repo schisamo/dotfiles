2fc89e0b246fd04b4c16f6ac0353abd05af0fef3
Seth Chisamore <schisamo@opscode.com>
2011-11-08 16:00:22 -0500

    updated chef stuff

--- bin/purge_instance.sh
+++ bin/purge_instance.sh
@@ -9,6 +9,22 @@
 # you should only use it if you understand the commands it runs.
 #
 ################################################################
+# if [[ $1 == '' ]]
+# then
+#   echo -n "Instance ID: "
+#   read instance
+# else
+#   instance=$1
+# fi
+# knife client delete $instance -y
+# knife node delete $instance -y
+# knife ec2 server delete $instance
+# if [ $? != 0 ]; then
+#   knife rackspace server delete $instance
+# fi
+# if [ $? != 0 ]; then
+#   knife euca server delete $instance
+# fi
 if [[ $1 == '' ]]
 then
   echo -n "Instance ID: "
@@ -16,6 +32,16 @@ then
 else
   instance=$1
 fi
-knife client delete $instance -y
-knife node delete $instance -y
-knife ec2 server delete $instance
+instance_ids=($instance)
+for id in ${instance_ids[@]}
+do
+  knife client delete $id -y
+  knife node delete $id -y
+  knife ec2 server delete $id
+  if [ $? != 0 ]; then
+    knife rackspace server delete $id
+  fi
+  if [ $? != 0 ]; then
+    knife euca server delete $id
+  fi
+done
--- chef/chef.symlink/knife.rb
+++ chef/chef.symlink/knife.rb
@@ -9,18 +9,23 @@ chef_server_url          "https://api.opscode.com/organizations/#{ENV['USER']}-d
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
-#cookbook_path ["/Users/#{ENV['USER']}/dev/github_#{ENV['USER']}/cookbooks"]
 
-cookbook_copyright "Opscode, Inc."
-cookbook_license "apachev2"
-cookbook_email "cookbooks@opscode.com"
+# cookbook_copyright "Opscode, Inc."
+# cookbook_license "apachev2"
+# cookbook_email "cookbooks@opscode.com"
+
+# encrypted data bags
+# encrypted_data_bag_secret "#{ENV['HOME']}/.chef/encrypted_data_bag_secret"
 
 # all your credentials are belong to us
+# require 'fog'
+# knife.merge! Fog.credentials
 
+#puts pp Fog.credentials
 # Amazon AWS
 knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
 
 # Rackspace Cloud
-knife[:rackspace_api_username] = "#{ENV['RACKSPACE_USERNAME']}"
-knife[:rackspace_api_key] = "#{ENV['RACKSPACE_API_KEY']}"
+knife[:rackspace_username] = ENV['RACKSPACE_USERNAME']
+knife[:rackspace_api_key] = ENV['RACKSPACE_API_KEY']
